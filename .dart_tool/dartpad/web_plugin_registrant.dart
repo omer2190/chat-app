94ae6e53fa21deee5c92a03776a5fd99a853a2ac
omer2190 <44607297+omer2190@users.noreply.github.com>
2023-11-24 14:21:26 +0300

    تصحيح الاصدار

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -12,6 +12,7 @@ import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -23,6 +24,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   FirebaseMessagingWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  ModalProgressHudNsnWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
